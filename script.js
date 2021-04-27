613cff3967dedd2a021939d4bc6d8f2aa7e565f7
Donnell Albertie <dalbertie5@gmail.com>
2021-04-27 16:50:42 -0500

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+1.
+for(var i=1; i<=20; i++){
+    if(i % 2==0);
+    else console.log(i)
+}
+2.
+var x=4
+for(var i=0; x > -3.5; i++){
+    x += -1.5;
+    console.log(x) 
+}
+3.
+var x=0
+for( var i=0; i<=100; i++){
+    x += i;
+    console.log(x)
+}
+4.
+var n=1
+while( n <= 1e8){
+    n*=100000;
+}
+console.log(n)
